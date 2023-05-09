ef5a77bce919519e9ecd593aef59bcc5a61876c4
yusufmuhumed <yusuf.muhumed@elev.ga.ntig.se>
2023-05-09 21:46:54 +0200

    jobb

--- validator.rb
+++ validator.rb
@@ -1,14 +1,14 @@
 def validator(options,user_input,message)
     
     i=0
-    p options[i]
+    
     while i < options.length
-        while options[i] != user_input
+       
             if options[i] == i
                 break
             end
             puts message   
-        end
+        
         i+=1
         user_input = gets.chomp.downcase
     end
